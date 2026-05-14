650890c320a81496a822ccc1da9f16a3bd0ac5b7
Mostafa_Hosny <tatahosny208@gmail.com>
2026-05-14 16:29:18 +0300

    Delete src directory

--- src/client/routes/routeConfig.js
+++
@@ -1,9 +0,0 @@
-export const ROLE_HOME_PATHS = {
-  super_admin: '/admin',
-  instructor: '/instructor',
-  mentor: '/mentor',
-  mentor_manager: '/mentor',
-  oc: '/hr',
-  hr: '/hr',
-  student: '/student',
-};
